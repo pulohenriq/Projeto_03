6c2170477c353c9272a02343903693a664f5a428
pulohenriq <paulolimalop06@gmail.com>
2026-04-22 22:35:06 -0300

    Add files via upload

---
+++ filablacklog.py
@@ -0,0 +1,26 @@
+class FilaBlacklog:
+    def ___int__(self):
+        self.dados = []
+    
+    def enqueue(self, jogo):
+        self.dados.append(jogo)
+        
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        return self.dados.pop(0)
+    
+    def is_empty(self):
+        return len(self.dados) == 0
+    
+    def mostrar(self):
+        if self.is_empty():
+            print('Blacklog vazio')
+            return
+        print('----- BACKLOG -----')
+        for index, jogo in enumerate(sef.dados, start= 1):
+            print(f'{index} - {jogo.exibir()}')
+            
+    def tamanho(self):
+        return len(self.dados)
+    
